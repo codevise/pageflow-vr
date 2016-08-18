cd748ee1936cd62d3807510c9362541e7c9c5e7b
Tim Fischbach <tfischbach@codevise.de>
2016-08-18 12:48:25 +0200

    Display fallback on IE <= 11

--- app/assets/javascripts/pageflow/vr/browser/vr_view_support.js
+++ app/assets/javascripts/pageflow/vr/browser/vr_view_support.js
@@ -1,3 +1,4 @@
 pageflow.browser.feature('vr view support', function(has) {
-  return has.not('ios platform');
+  return !pageflow.browser.agent.matchesMobileSafari() &&
+    !pageflow.browser.agent.matchesIEUpTo11();
 });
