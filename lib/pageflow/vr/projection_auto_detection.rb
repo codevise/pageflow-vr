46295320c7d747df0dc1c75de34154a3f801ad9a
Tim Fischbach <tfischbach@codevise.de>
2017-01-04 17:02:07 +0100

    Adapt projection autodetection to new file usage config
    File config is now stored on the usage instead of the file itself.

--- lib/pageflow/vr/projection_auto_detection.rb
+++ lib/pageflow/vr/projection_auto_detection.rb
@@ -4,9 +4,10 @@ module Pageflow
       def call(options)
         file = options[:file]
         return unless file.is_a?(VideoFile)
+        usage = file.usages.first
 
-        if dimension_present?(file) && auto_detection_enabled?(file)
-          update(file)
+        if dimension_present?(file) && auto_detection_enabled?(usage)
+          update(usage, file)
         end
       end
 
@@ -16,15 +17,15 @@ module Pageflow
         video_file.width.present? && video_file.height.present?
       end
 
-      def auto_detection_enabled?(video_file)
-        video_file.configuration['projection'] == 'auto_detect'
+      def auto_detection_enabled?(file_usage)
+        file_usage.configuration['projection'] == 'auto_detect'
       end
 
-      def update(video_file)
-        video_file.configuration =
-          video_file.configuration.merge(projection: detect_projection(video_file))
+      def update(usage, video_file)
+        usage.configuration =
+          usage.configuration.merge(projection: detect_projection(video_file))
 
-        video_file.save!
+        usage.save!
       end
 
       def detect_projection(video_file)
