1aa2c5a831ed6149f812f739ce94d7836220b3f5
Tim Fischbach <tfischbach@codevise.de>
2017-10-10 15:51:00 +0200

    Allow displaying vrview in embedded entry
    Remove X-Frame-Options header.

--- app/controllers/pageflow/vr/static_files_controller.rb
+++ app/controllers/pageflow/vr/static_files_controller.rb
@@ -1,12 +1,20 @@
 module Pageflow
   module Vr
     class StaticFilesController < ActionController::Base
+      after_action :allow_iframe, only: :vrview
+
       def vrview
         respond_to do |format|
           format.html
           format.css
           format.js
         end
+      end
+
+      private
+
+      def allow_iframe
+        response.headers.except! 'X-Frame-Options'
       end
     end
   end
