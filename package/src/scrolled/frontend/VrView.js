08ee6ca1eb0cf070ea0ba2a0fc5aa2710d3ecc81
Tim Fischbach <tfischbach@codevise.de>
2020-08-05 17:55:01 +0200

    Render vrview iframe in VrView component
    REDMINE-17383

--- package/src/scrolled/frontend/VrView.js
+++ package/src/scrolled/frontend/VrView.js
@@ -1,3 +1,80 @@
-export function VrView() {
-  return 'VR View';
+import React, {useRef} from 'react';
+
+import {
+  useContentElementEditorState,
+  useContentElementLifecycle,
+  useFile,
+  Figure,
+  ViewportDependentPillarBoxes
+} from 'pageflow-scrolled/frontend';
+
+import styles from './VrView.module.css';
+
+export function VrView({configuration}) {
+  const {isPrepared} = useContentElementLifecycle({
+    onActivate() {
+      if (frameRef.current) {
+        frameRef.current.contentWindow.postMessage({type: 'autopan'},
+                                                   window.location.origin);
+      }
+    }
+  });
+  const {isEditable, isSelected} = useContentElementEditorState();
+  const frameRef = useRef();
+
+  const imageFile = useFile({collectionName: 'imageFiles', permaId: configuration.image});
+
+  return (
+    <div style={{pointerEvents: isEditable && !isSelected ? 'none' : undefined}}>
+      <Figure caption={configuration.caption}>
+        <ViewportDependentPillarBoxes aspectRatio={configuration.position === 'full' ? 0.5 : 0.75}
+                                      position={configuration.position}
+                                      opaque={!!configuration.caption}>
+          {renderIframeIfPrepared(configuration, imageFile, isPrepared, frameRef)}
+        </ViewportDependentPillarBoxes>
+      </Figure>
+    </div>
+  );
+}
+
+function renderIframeIfPrepared(configuration, imageFile, isPrepared, frameRef) {
+  const source = getSource(imageFile, configuration)
+
+  if (isPrepared && source) {
+    return renderIframe(source, frameRef);
+  }
+}
+
+function renderIframe(source, frameRef) {
+  return (
+    <iframe ref={frameRef}
+            className={styles.view}
+            allowFullScreen
+            frameBorder="0"
+            src={source}>
+    </iframe>
+  );
+}
+
+function getSource(imageFile, {startYaw}) {
+  if (!imageFile || !imageFile.isReady) {
+    return null;
+  }
+
+  return url({
+    image: imageFile.urls.ultra,
+    is_stereo: imageFile.configuration.projection == 'equirectangular_stereo' ? 'true' : 'false',
+    start_yaw: startYaw,
+    is_vr_off: true,
+    is_autopan_off: true,
+    no_autoplay: true
+  });
+}
+
+function url(params) {
+  const paramsString = Object.keys(params).map((key) =>
+    `${key}=${params[key]}`
+  ).join('&');
+
+  return `/pageflow_vr/vrview2.html?${paramsString}`;
 }
